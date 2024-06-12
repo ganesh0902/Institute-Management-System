7a9be61974ee77531162cda40497f6ff392742bb
Ganesh sakhare <101263428+ganeshsakhare0902@users.noreply.github.com>
2024-06-12 21:49:45 +0530

    added identity service for spring security

---
+++ identity-service/src/main/java/com/identity/controller/controller.java
@@ -0,0 +1,11 @@
+package com.identity.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class controller {
+
+	
+}
